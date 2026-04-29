578c29e5638acb74ade77b4a1b3e9f135f2e8d58
martina-murga <mmurga@mail.austral.edu.ar>
2026-04-29 00:33:29 -0300

    Update exercise_is_empty.py

--- exercise_is_empty.py
+++ exercise_is_empty.py
@@ -10,7 +10,7 @@ def is_empty(lista):
     Returns:
         True si la lista está vacía, False en caso contrario
     """
-    if lista== []:
+    if lista == []:
         return True
     else:
         return False
